802e6fab845364195b04d55740c843408e41fa83
chanchal <achanchal1973@gmail.com>
2026-05-17 23:25:34 +0530

    Update API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://goal-tracker-f5iz.onrender.com', // your Render backend URL
 });
 
 api.interceptors.request.use((config) => {
